6124d0c682a5ec75b339d4aafc8145b1accc9611
Johnny Qu <johnnyqu4@gmail.com>
2020-04-06 05:58:39 -0400

    fixed death message system

--- msgGen.cpp
+++ msgGen.cpp
@@ -5,11 +5,17 @@ int main(int argc, char *argv[]){
     if(argc < 2){
         cout << "call this program with the death message you want typed out" << endl;
     }else{
-        for(int i = 0; i < strlen(argv[1]); i++){
-            for(int j = 0; j <= i; j++){
-                cout << argv[1][j];
+        for(int z = 1; z < argc; z++){
+            for(int i = 0; i < strlen(argv[z]); i++){
+                for(int k = 1; k < z; k++){
+                    cout << argv[k];
+                    cout << " ";
+                }
+                for(int j = 0; j <= i; j++){
+                    cout << argv[z][j];
+                }
+                cout << endl;
             }
-            cout << endl;
         }
     }
     
